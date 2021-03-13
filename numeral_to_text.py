92b3601e19b0b92d8e7c418a5fc307276ba305a8
mrosas91 <marcorosas1991@icloud.com>
2021-03-13 11:07:05 -0700

    Add function to convert numerals to nahuatl text. Support up to 19

--- numeral_to_text.py
+++ numeral_to_text.py
@@ -11,3 +11,21 @@ root_400 = 'tsontli'
 # root_160000
 
 connector = ' iuan '
+
+def numeral_to_text(numeral):
+	return numeral_to_text(numeral, True)
+
+def numeral_to_text(numeral, first_time):
+	possible_connector = ''
+	if not first_time:
+		possible_connector = connector
+		if numeral == 0:
+			return ''
+
+	if numeral < 10:
+		return possible_connector + root_names_to_9[numeral]
+	elif numeral <20 :
+		return root_10 + numeral_to_text(numeral-10, False)
+	
+	else:
+		return 'UNKNOWN'
