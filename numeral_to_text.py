ba8a13d902f4b90f629b5d5a8c00f6cdc2a5156f
mrosas91 <marcorosas1991@icloud.com>
2021-03-13 11:33:53 -0700

    Support numbers up to 399. Phase 1.

--- numeral_to_text.py
+++ numeral_to_text.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-prefixes = ['sem', 'om', 'ex', 'nau', 'makuil']
+# prefixes array uses effectively index 1 to store prefixes
+prefixes = ['-', 'sem', 'om', 'ex', 'naj', 'makuil', '6', '7', '8', '9', 'matlak']
 
-root_names_to_9 = ['amopouali',  'se', 'ome', 'eyi', 'naui', 'makuilli', 'chikuase', 'chikome', 'chikueyi', 'chiknaui']
+root_names_to_9 = ['amopouali',  'se', 'ome', 'eyi', 'naui', 'makuili', 'chikuasej', 'chikome', 'chikueyi', 'chiknaui']
 
-root_10 = 'majtlactli'
-root_20 = 'poualli'
+root_10 = 'majtlaktli'
+root_20 = 'pouali'
 root_400 = 'tsontli'
-# root_8000
+root_8000 = 'xiquipilli'
 # root_160000
 
 connector = ' iuan '
@@ -25,7 +26,12 @@ def numeral_to_text(numeral, first_time):
 	if numeral < 10:
 		return possible_connector + root_names_to_9[numeral]
 	elif numeral <20 :
-		return root_10 + numeral_to_text(numeral-10, False)
-	
+		return possible_connector + root_10 + numeral_to_text(numeral-10, False)
+	elif numeral < 400:
+		times_in_20 = numeral // 20
+		return possible_connector + prefixes[times_in_20] + root_20 + numeral_to_text(numeral-(times_in_20 * 20), False)
+	# elif numeral < 8000 :
+	# 	return 'sentsontli'
+
 	else:
 		return 'UNKNOWN'
