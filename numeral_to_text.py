b048c6a31e4e9769ceaa74e5645ad74b1c38c6fb
mrosas91 <marcorosas1991@icloud.com>
2021-03-13 11:06:18 -0700

    Add root names and prefixes

---
+++ numeral_to_text.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+prefixes = ['sem', 'om', 'ex', 'nau', 'makuil']
+
+root_names_to_9 = ['amopouali',  'se', 'ome', 'eyi', 'naui', 'makuilli', 'chikuase', 'chikome', 'chikueyi', 'chiknaui']
+
+root_10 = 'majtlactli'
+root_20 = 'poualli'
+root_400 = 'tsontli'
+# root_8000
+# root_160000
+
+connector = ' iuan '
